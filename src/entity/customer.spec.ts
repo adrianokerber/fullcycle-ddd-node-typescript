bd1fef0e545f1680fdc765d0fff00f6f4517f9e5
Adriano Kerber <kerberpro@gmail.com>
2023-09-19 17:32:03 -0300

    ✅ - Add Customer tests

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,6 +1,72 @@
+import Address from "./address";
+import Customer from "./customer";
+
 describe("Customer unit tests", () => {
-    it("should get 1 as result", () => {
-        const result = 1;
-        expect(result).toBe(1);
+
+    it("should throw an error when the ID is empty", () => {
+
+        expect(() => {
+            let customer = new Customer("", "John");
+        }).toThrowError("Id is required");
+
     });
+
+    it("should throw an error when the NAME is empty", () => {
+
+        expect(() => {
+            let customer = new Customer("123", "");
+        }).toThrowError("Name is required");
+
+    });
+
+    it("should change name", () => {
+
+        // Arrange
+        const customer = new Customer("123", "John");
+
+        // Act
+        customer.changeName("Jane");
+
+        // Assert
+        expect(customer.name).toBe("Jane");
+
+    });
+
+    it("should activate the customer", () => {
+
+        // Arrange
+        const customer = new Customer("1", "Customer 1");
+        const address = new Address("Street 1", 123, "12341251", "São Paulo");
+        customer.address = address;
+
+        // Act
+        customer.activate();
+
+        // Assert
+        expect(customer.isActive()).toBe(true);
+
+    });
+
+    it("should deactivate the customer", () => {
+
+        // Arrange
+        const customer = new Customer("1", "Customer 1");
+
+        // Act
+        customer.deactivate();
+
+        // Assert
+        expect(customer.isActive()).toBe(false);
+
+    });
+
+    it("should throw an error when we try to activate and the address is not set", () => {
+
+        expect(() => {
+            const customer = new Customer("1", "Customer 1");
+            customer.activate();
+        }).toThrowError("Address is mandatory to activate a customer");
+
+    });
+
 });
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,10 +2,10 @@ import Address from "./address";
 
 export default class Customer {
     
-    _id: string;
-    _name: string;
-    _address!: Address;
-    _active: boolean = false;
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+    private _active: boolean = false;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -15,11 +15,11 @@ export default class Customer {
     }
 
     validate() {
-        if (this._name.length === 0) {
-            throw new Error("Name is required");
-        }
         if (this._id.length === 0) {
             throw new Error("Id is required");
+        }
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
         }
     }
 
@@ -41,6 +41,8 @@ export default class Customer {
 
     changeName(name: string) {
         this._name = name;
+        
+        this.validate();
     }
 
     activate() {
@@ -52,5 +54,9 @@ export default class Customer {
 
     deactivate() {
         this._active = false;
+    }
+
+    isActive(): boolean {
+        return this._active;
     }
 }
