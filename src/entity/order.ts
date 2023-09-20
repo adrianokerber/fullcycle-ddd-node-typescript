eb34847207ccc12e4c251d1d8234aea115f16642
Adriano Kerber <kerberpro@gmail.com>
2023-09-20 09:02:57 -0300

    🧪 - Add a failling test for Order entity

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -2,10 +2,10 @@ import OrderItem from "./order_item";
 
 export default class Order {
 
-    _id: string;
-    _customerId: string;
-    _items: OrderItem[];
-    _total: number;
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[];
+    private _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
