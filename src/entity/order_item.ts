ebb15cb8be2806bb4e12cd2bc26fc2fd71b3d024
Adriano Kerber <kerberpro@gmail.com>
2023-09-21 16:22:40 -0300

    🦺 - Add initial validation to OrderItem.ts and some tests

--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,14 +1,32 @@
 export default class OrderItem {
 
-    _id: string;
-    _name: string;
-    _price: number;
-    _quantity: number;
+    private _id: string;
+    private _productId: string;
+    private _name: string;
+    private _price: number;
+    private _quantity: number;
 
-    constructor(id: string, name: string, price: number, quantity: number) {
+    constructor(id: string, name: string, price: number, productId: string, quantity: number) {
         this._id = id;
+        this._productId = productId;
         this._name = name;
         this._price = price;
         this._quantity = quantity;
+
+        this.validate();
+    }
+
+    validate() {
+        if (this._quantity <= 0) {
+            throw new Error("Quantity must be greater than zero");
+        }
+    }
+
+    get price(): number {
+        return this._price;
+    }
+
+    orderItemTotal(): number {
+        return this._price * this._quantity;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Rua dois", 2, "123454657", "São Paulo");
 customer.address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10, 1);
-const item2 = new OrderItem("2", "Item 2", 10, 1);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 1);
+const item2 = new OrderItem("2", "Item 2", 10, "p1", 1);
 
 const order = new Order("1", "123", [item1, item2]);
