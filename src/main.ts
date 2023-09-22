77d24cfb4a9e3e66c1087f816fecc0ffe246b18d
Adriano Kerber <kerberpro@gmail.com>
2023-09-22 14:55:40 -0300

    🚚 - Improve project files structure and add repository-interface.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/order_item";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
+import Order from "./domain/entity/order";
+import OrderItem from "./domain/entity/order_item";
 
 let customer = new Customer("123", "Whesley Willans");
 const address = new Address("Rua dois", 2, "123454657", "São Paulo");
