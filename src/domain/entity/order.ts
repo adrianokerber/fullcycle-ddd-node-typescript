995ddc2c58c354828b51a7b06b4ab4c76125d19f
Adriano Kerber <kerberpro@gmail.com>
2023-09-23 12:19:31 -0300

    ✨ - Create OrderRepository and implement create method

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -30,6 +30,18 @@ export default class Order {
         return true;
     }
 
+    get id(): string {
+        return this._id;
+    }
+
+    get customerId(): string {
+        return this._customerId;
+    }
+
+    get items(): OrderItem[] {
+        return this._items;
+    }
+
     total(): number {
         return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
     }
--- src/domain/entity/order_item.ts
+++ src/domain/entity/order_item.ts
@@ -22,8 +22,24 @@ export default class OrderItem {
         }
     }
 
+    get id(): string {
+        return this._id;
+    }
+
+    get productId(): string {
+        return this._productId;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
     get price(): number {
         return this._price;
+    }
+
+    get quantity(): number {
+        return this._quantity;
     }
 
     orderItemTotal(): number {
