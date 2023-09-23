8917474070e28a6d926daa7b336ce7b11fe1690c
Adriano Kerber <kerberpro@gmail.com>
2023-09-23 02:35:16 -0300

    🗃️ - Adds Order aggregate ORMs

---
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize-typescript';
+import CustomerModel from '../db/sequelize/model/customer.model';
+
+describe("OrderRepository test", () => {
+
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            logging: false,
+            sync: { force: true },
+        });
+
+        sequelize.addModels([CustomerModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+});
