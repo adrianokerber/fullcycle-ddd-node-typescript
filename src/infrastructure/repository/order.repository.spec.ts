0e59e89740fce3bd1e061e600f7eef0c250a636b
Adriano Kerber <kerberpro@gmail.com>
2023-09-23 12:19:31 -0300

    ✨ - Create OrderRepository and implement create method

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -1,5 +1,16 @@
 import { Sequelize } from 'sequelize-typescript';
 import CustomerModel from '../db/sequelize/model/customer.model';
+import OrderModel from '../db/sequelize/model/order.model';
+import OrderItemModel from '../db/sequelize/model/order-item.model';
+import ProductModel from '../db/sequelize/model/product.model';
+import CustomerRepository from './customer.repository';
+import Customer from '../../domain/entity/customer';
+import Address from '../../domain/entity/address';
+import ProductRepository from './product.repository';
+import Product from '../../domain/entity/product';
+import OrderItem from '../../domain/entity/order_item';
+import Order from '../../domain/entity/order';
+import OrderRepository from './order.repository';
 
 describe("OrderRepository test", () => {
 
@@ -13,12 +24,60 @@ describe("OrderRepository test", () => {
             sync: { force: true },
         });
 
-        sequelize.addModels([CustomerModel]);
+        sequelize.addModels([CustomerModel, OrderModel, OrderItemModel, ProductModel]);
         await sequelize.sync();
     });
 
     afterEach(async () => {
         await sequelize.close();
+    });
+
+    it("should create a new order", async () => {
+
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer("c1", "Customer 1");
+        const address = new Address("Street 1", 1, "ZIP code 1", "City 1");
+        customer.changeAddress(address);
+        await customerRepository.create(customer);
+
+        const productRepository = new ProductRepository();
+        const product = new Product("p1", "Product 1", 10);
+        await productRepository.create(product);
+
+        const item = new OrderItem(
+            "i1",
+            product.name,
+            product.price,
+            product.id,
+            2
+        );
+
+        const order = new Order("o1", customer.id, [item]);
+
+        const orderRepository = new OrderRepository();
+        await orderRepository.create(order);
+
+        const orderModel = await OrderModel.findOne({
+            where: { id: "o1" },
+            include: ["items"],
+        });
+
+        expect(orderModel?.toJSON()).toStrictEqual({
+            id: "o1",
+            customer_id: "c1",
+            total: order.total(),
+            items: [
+                {
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    quantity: item.quantity,
+                    order_id: "o1",
+                    product_id: "p1",
+                },
+            ],
+        });
+
     });
 
 });
---
+++ src/infrastructure/repository/order.repository.ts
@@ -0,0 +1,37 @@
+import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
+
+export default class OrderRepository implements OrderRepositoryInterface {
+
+    async create(entity: Order): Promise<void> {
+        await OrderModel.create(
+            {
+                id: entity.id,
+                customer_id: entity.customerId,
+                total: entity.total(),
+                items: entity.items.map((item) => ({
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    product_id: item.productId,
+                    quantity: item.quantity,
+                }))
+            },
+            {
+                include: [{model: OrderItemModel}]
+            }
+        );
+    }
+
+    update(entity: Order): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    find(id: string): Promise<Order> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<Order[]> {
+        throw new Error("Method not implemented.");
+    }
+}
