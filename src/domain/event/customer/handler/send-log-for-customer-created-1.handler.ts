278b13859d5713a1c94f6974dc9b21b4cd3f404b
Adriano Kerber <kerberpro@gmail.com>
2023-09-26 15:30:25 -0300

    ♻️ - Revamp event handlers of Customer to math English language instead of Brazilian Portuguese

---
+++ src/domain/event/customer/handler/send-log-for-customer-created-1.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class SendLogForCustomerCreated1Handler implements EventHandlerInterface<CustomerCreatedEvent> {
+
+    handle(event: CustomerCreatedEvent): void {
+        console.log(`This is the first console.log() of the event "CustomerCreatedEvent" displayed by the "handler_1"`);
+    }
+
+}
